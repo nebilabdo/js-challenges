372cae9efcea24c72e9388535ad544e53e20f92e
Nebil <nebilbromance@gmail.com>
2025-06-04 13:48:51 +0300

    remove empty strings from array using filter

---
+++ string/remove-empty-strings.js
@@ -0,0 +1,4 @@
+const words = ["pen", "", "pencil", "", "book"];
+const removeEmptyString=words.filter(data=>data)
+console.log(removeEmptyString)
+//filter only gives the truth part only that means emphty is the false value so it does not returned
