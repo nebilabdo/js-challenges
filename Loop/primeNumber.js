ea78ca594a456f2432ea58fd17295a9417e9ac47
Nebil <nebilbromance@gmail.com>
2025-07-25 01:34:45 -0700

    Update primeNumber.js
    Add another way to solve prime number

--- Loop/primeNumber.js
+++ Loop/primeNumber.js
@@ -13,3 +13,15 @@ for(let n=2;n<10;n++){
         }
     
 }
+
+// in other methods  
+let n = 10;
+
+nextPrime:
+for (let i = 2; i <= n; i++) {     
+
+  for (let j = 2; j < i; j++) {    
+    if (i % j == 0) continue nextPrime; 
+
+  alert(i);  
+}
