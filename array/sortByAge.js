44890466bbecbbdcfcda1af76f2c6223b1998037
Nebil <nebilbromance@gmail.com>
2025-10-17 07:36:07 +0300

    Add sortByAge function to sort users by age
    Implement sorting of an array of objects by age.

---
+++ array/sortByAge.js
@@ -0,0 +1,13 @@
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
+
+let arr = [ pete, john, mary ];
+let sortByAge=arr.sort((a,b)=>a.age-b.age)
+
+console.log(sortByAge)
+
+// now: [john, mary, pete]
+console.log(arr[0].name); // John
+console.log(arr[1].name); // Mary
+console.log(arr[2].name); // Pete
