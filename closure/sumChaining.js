18033c42236936a9e8731418f69e21104421bee0
Nebil <nebilbromance@gmail.com>
2025-11-09 09:13:29 +0300

    Implement recursive sum function for chaining
    Refactor sum function to use recursion for chaining.

--- closure/sumChaining.js
+++ closure/sumChaining.js
@@ -20,3 +20,23 @@ console.log(+sum(1)(2)(3)); // 6
 console.log(+sum(5)(-1)(2)); // 6
 console.log(+sum(6)(-1)(-2)(-3)); // 0
 console.log(+sum(0)(1)(2)(3)(4)(5)); // 15
+
+//Using recursion 
+function sum(a) {
+  function f(b) {
+    // Recursive call: add b to a and return a new function expecting next value
+    return sum(a + b);
+  }
+
+  // Convert function to primitive so we can see the result that means either operator or alert 
+  f.toString = function() {
+    return a;
+  };
+
+  return f;
+}
+
+console.log(+sum(1)(2));        // 3
+console.log(sum(1)(2)(3).toString());     // 6
+console.log(+sum(5)(-1)(2));    // 6
+console.log(+sum(6)(-1)(-2)(-3)); // 0
