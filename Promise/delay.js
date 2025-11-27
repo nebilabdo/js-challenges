54294983118bfd15fe36c7c735a1c27416d6cc04
Nebil <nebilbromance@gmail.com>
2025-11-27 12:27:55 +0300

    Create delay.js

---
+++ Promise/delay.js
@@ -0,0 +1,17 @@
+function delay(ms) {
+  // your code
+  return new Promise(function(resolve,reject){
+     setTimeout(()=>resolve(),ms)
+  })
+}
+
+delay(3000).then(() => console.log('runs after 3 seconds'));
+
+
+// to make even shorter 
+function delay(ms) {
+  // your code
+  return new Promise(resolve=>setTimeout(resolve,ms))
+}
+
+delay(3000).then(() => console.log('runs after 3 seconds'));
