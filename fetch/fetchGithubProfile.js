c5e947a5a329a416349a9592dd3b3071c99d31b5
Nebil <192956494+nebilabdo@users.noreply.github.com>
2025-12-05 07:55:12 +0300

    Add function to fetch and show GitHub avatar
    Fetch and display GitHub user's avatar for 3 seconds.

---
+++ fetch/fetchGithubProfile.js
@@ -0,0 +1,12 @@
+fetch(`https://api.github.com/users/nebilabdo`)
+  // Load the response as json
+  .then(response => response.json())
+  // Show the avatar image (githubUser.avatar_url) for 3 seconds (maybe animate it)
+  .then(githubUser => {
+    let img = document.createElement('img');
+    img.src = githubUser.avatar_url;
+    img.className = "promise-avatar-example";
+    document.body.append(img);
+   return img;
+  }).then(img=>setTimeout(() => img.remove(), 3000) // (*)
+)
