a95d492d3e30f8675d3bad22df04555189260349
Nebil <192956494+nebilabdo@users.noreply.github.com>
2025-12-13 15:33:00 +0300

    Create HTML table with sorting script

---
+++ createTable.js
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table>
+<thead>
+  <tr>
+    <th>Name</th><th>Surname</th><th>Age</th>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <td>John</td><td>Smith</td><td>10</td>
+  </tr>
+  <tr>
+    <td>Pete</td><td>Brown</td><td>15</td>
+  </tr>
+  <tr>
+    <td>Ann</td><td>Lee</td><td>5</td>
+  </tr>
+  <tr>
+    <td>Wow</td><td>perfect</td><td>3</td>
+  </tr>
+    <tr>
+    <td>...</td><td>...</td><td>...</td>
+  </tr>
+</tbody>
+</table>
+<script>
+// let tableBody=document.querySelector('tbody');
+// for(let x=0; x<tableBody.children.length;x++){
+//     for(let y=x+1;y<tableBody.children.length;y++){
+//   let firstValue=tableBody.children[x].cells[0].textContent;
+//   let secondValue=tableBody.children[y].cells[0].textContent;
+
+//  if(firstValue>secondValue){
+//     let temp=tableBody.children[x].innerHTML;
+// tableBody.children[x].innerHTML=tableBody.children[y].innerHTML;
+// tableBody.children[y].innerHTML=temp;
+//  }
+// }
+// }
+let table = document.querySelector('table');
+let sortedRows = Array.from(table.tBodies[0].rows) // 1
+  .sort((rowA, rowB) => rowA.cells[0].innerHTML.localeCompare(rowB.cells[0].innerHTML));
+table.tBodies[0].append(...sortedRows); 
+</script>
+</body>
+</html>
